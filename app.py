d26cd815888909a0273c68892cd6aa5d7dadd762
taoli2014 <tao.li2010@gmail.com>
2024-04-12 08:28:19 +0000

    intermediate

---
+++ app.py
@@ -0,0 +1,72 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.write("Hello world")
+
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+st.dataframe(dataframe.style.highlight_max(axis=0))
+
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+st.table(dataframe)
+
+
+left_column, right_column = st.columns(2)
+# You can use a column just like st.sidebar:
+left_column.button('Press me!')
+
+# Or even better, call Streamlit functions inside a "with" block:
+with right_column:
+    chosen = st.radio(
+        'Sorting hat',
+        ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
+    st.write(f"You are in {chosen} house!")
+
+st.write("Line Curve")
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+st.line_chart(chart_data)
+
+
+x = st.slider('x')  # this is a widget
+st.write(x, 'squared is', x * x)
+
+st.text_input("Your name", key="name")
+# You can access the value at any point with:
+st.session_state.name
+
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
+    
+    chart_data
+
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+    })
+
+option = st.selectbox(
+    'Which number do you like best?',
+     df['first column'])
+
+'You selected: ', option
+
+
+# Add a selectbox to the sidebar:
+add_selectbox = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone')
+)
+
+# Add a slider to the sidebar:
+add_slider = st.sidebar.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0)
+)
--- content/blog/fico.md
+++ content/blog/fico.md
@@ -6,7 +6,7 @@ categories = ["news"]
 authors = ["Zoldi"]
 +++
 
-The single-layer <mark>NN</mark> for fraud.  
+The single-layer <mark>NN</mark> for fraud. H<sub>2</sub>O.   
 
 [Zoldi](https://www.fico.com/blogs/author/scott-zoldi)
 
@@ -28,6 +28,28 @@ Inline math is generated if you use a single `$` as a delimiter around your form
 
 
 Try the footnote [^1], another one [^3].
+
+- [x] Basic Test
+- [ ] More Tests
+  - [x] View
+  - [x] Hear
+  - [ ] Smell
+
+Be impressed by my advanced code:
+
+    // Some comments
+    line 1 of code
+    line 2 of code
+    line 3 of code
+
+| Option | Description |
+|--------|-------------|
+| data   | path to data files to supply the data that will be passed into templates. |
+| engine | engine to be used for processing templates. Handlebars is the default. |
+| ext    | extension to be used for dest files. |
+
+[Upstage](https://github.com/upstage/ "Visit Upstage!")
+
 
 [^1]: This is the first footnote.
 [^3]: Does it work?
